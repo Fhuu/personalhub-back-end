4064f4742ea1e7cf2f8de6083c6702379ff76109
s0566367 <diro.baloska@student.htw-berlin.de>
2020-07-31 20:03:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.listen(port, () => {
+    console.log("up and running!");
+})
+
+app.get("/", (req,res) => {
+    res.send("Up and running!");
+})
+
 const mongoose = require("mongoose");
 const dbURL = process.env.MONGODB_URI || "mongodb://localhost:27017/personalhubdb";
 mongoose.connect(dbURL, {useNewUrlParser : true, useUnifiedTopology : true, useFindAndModify : true})
@@ -13,10 +21,3 @@ mongoose.connect(dbURL, {useNewUrlParser : true, useUnifiedTopology : true, useF
         console.log(error);
     });
 
-app.listen(port, () => {
-    console.log("up and running!");
-})
-
-app.get("/", (req,res) => {
-    res.send("Up and running!");
-})
