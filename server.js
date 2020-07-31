a21f7d87ae9b8d10a776d9dc7ff12949e1142d6d
s0566367 <diro.baloska@student.htw-berlin.de>
2020-07-31 17:08:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log("up and running!");
 })
-yar
+
 app.get("/", (req,res) => {
     res.send("Up and running!");
 })
