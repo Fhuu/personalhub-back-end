50e640567220c08684eecd2c01e4481c37b2784b
s0566367 <diro.baloska@student.htw-berlin.de>
2020-07-31 17:12:42 +0200

    add database with cluster

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+
+const mongoose = require("mongoose");
+const dbURL = "mongodb+srv://adminuser:useradmin@fhuuka.hccpa.mongodb.net/<dbname>?retryWrites=true&w=majority";
+mongoose.connect(dbURL, {useNewUrlParser : true, useUnifiedTopology : true, useFindAndModify : true})
+    .then(() => {
+        console.log("connected to database on fhuuka cluster");
+    })
+    .catch(() => {
+        console.log("error connecting to database on fhuuka cluster");
+    });
+
 app.listen(port, () => {
     console.log("up and running!");
 })
