b6de491d4216cfdb02ab22a545acdf6453098995
s0566367 <diro.baloska@student.htw-berlin.de>
2020-08-12 17:13:51 +0200

    session setup

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+//----------------Server setup-----------------//
 app.listen(port, () => {
     console.log("up and running!");
 })
@@ -10,6 +12,23 @@ app.get("/", (req,res) => {
     res.send("Up and running!");
 })
 
+//------------------Session setup------------------//
+const session = require('express-session');
+app.set('trust proxy', 1);
+app.use(session({
+    genid: function(req) {
+        return genuuid()
+    },
+    secret: 'secret',
+    resave: false,
+    saveUninitialized : true,
+    cookie : {
+        secure : "auto",
+        maxAge: 360000
+    }
+}))
+
+//----------------------Database---------------------------//
 const mongoose = require("mongoose");
 const dbURL = process.env.MONGODB_URI || "mongodb://localhost:27017/personalhubdb";
 mongoose.connect(dbURL, {useNewUrlParser : true, useUnifiedTopology : true, useFindAndModify : true})
@@ -21,3 +40,7 @@ mongoose.connect(dbURL, {useNewUrlParser : true, useUnifiedTopology : true, useF
         console.log(error);
     });
 
+//------------------------Session--------------------------//
+
+
+//------------------------Passport-------------------------//
