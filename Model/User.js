609ff2f8bf032b87aa3daaea5fa1ce474b6d7f68
s0566367 <diro.baloska@student.htw-berlin.de>
2020-08-12 17:26:19 +0200

    passport plugin to user model

--- Model/User.js
+++ Model/User.js
@@ -1,4 +1,18 @@
 const Mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose")
 const userSchema = Mongoose.Schema({
-    
+    username : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true
+    }
 })
+
+userSchema.plugin(passportLocalMongoose, {
+    usernameField : "username"
+});
+
+module.exports = Mongoose.model("user", userSchema);
