cfea6d70cd6dedb08cf039e23c4e672ae8baeba5
s0566367 <diro.baloska@student.htw-berlin.de>
2020-08-22 17:20:18 +0200

    change back using cors package for cors handling midleware

--- configs/cors-config.js
+++ configs/cors-config.js
@@ -24,14 +24,15 @@ const corsConfig = (app) => {
         ]
     });
 
+    app.use(cors(corsSetting));
 
-    app.use((req,res,next) => {
-        res.header('Access-Control-Allow-Credentials', true);
-        res.header('Access-Control-Allow-Origin', origins[3]);
-        res.header('Access-Control-Allow-Methods', 'GET, POST');
-        res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-        next();
-    });
+    // app.use((req,res,next) => {
+    //     res.header('Access-Control-Allow-Credentials', true);
+    //     res.header('Access-Control-Allow-Origin', origins[3]);
+    //     res.header('Access-Control-Allow-Methods', 'GET, POST');
+    //     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    //     next();
+    // });
 
 }
 module.exports = corsConfig;
