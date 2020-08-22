2d677ba20067fd2c053405a7dcb23a7b10a7621d
s0566367 <diro.baloska@student.htw-berlin.de>
2020-08-22 10:27:09 +0200

    cors without using cors module dependency

--- configs/cors-config.js
+++ configs/cors-config.js
@@ -6,7 +6,7 @@ const corsConfig = (app) => {
         'http://localhost:3000'
     ];
     const cors = require("cors");
-    app.use(cors({
+    const corsSetting = cors({
         origin : origins,
         optionsSuccessStatus : 200,
         credentials : true,
@@ -22,7 +22,15 @@ const corsConfig = (app) => {
             "DELETE",
             "PUT"
         ]
-    }));
+    });
+
+
+    app.use((req,res,next) => {
+        res.header('Access-Control-Allow-Credentials', true);
+        res.header('Access-Control-Allow-Origin', origins);
+        res.header('Access-Control-Allow-Methods', 'GET, POST');
+        res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    });
 
 }
 module.exports = corsConfig;
