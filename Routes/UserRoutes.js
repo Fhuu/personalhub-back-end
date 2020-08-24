dd104b104431dbb83e71b5d886ad59ff57fb635a
s0566367 <diro.baloska@student.htw-berlin.de>
2020-08-24 17:56:47 +0200

    update middle ware for create account

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -4,7 +4,7 @@ const controller = require("../Controller/userController");
 const passport = require("passport");
 
 router.get("/test", controller.try);
-router.post("/create", controller.create, passport.authenticate('local'))
+router.post("/create", controller.create, passport.authenticate('local'), controller.checkLogin)
 router.post("/login", passport.authenticate('local'), controller.checkLogin);
 router.get("/status", controller.checkLogin);
 router.get("/logout", controller.logout);
